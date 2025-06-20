9814196c0702879fe718bdb636f9ba2c949056fa
tahsin2627 <tahsinhassan280@gmail.com>
2025-06-20 11:43:54 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const leftDoor = document.querySelector('.left');
+    const rightDoor = document.querySelector('.right');
+
+    leftDoor.classList.add('open');
+    rightDoor.classList.add('open');
+});
